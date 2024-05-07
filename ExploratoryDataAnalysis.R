8aa318bd9da5ab43f67e3e3e26a4c6081bc07fcc
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-07 14:36:00 +0300

    Measures of frequency

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -17,3 +17,24 @@ head(salary_data)
 
 # View the dataset in a separate viewer window
 View(salary_data)
+
+# Measures of Frequency
+# Frequency table for Education
+education_freq <- table(salary_data$Education)
+print("Frequency table for Education:")
+print(education_freq)
+
+# Frequency table for Location
+location_freq <- table(salary_data$Location)
+print("Frequency table for Location:")
+print(location_freq)
+
+# Frequency table for Job_Title
+job_title_freq <- table(salary_data$Job_Title)
+print("Frequency table for Job_Title:")
+print(job_title_freq)
+
+# Summary of Gender
+gender_summary <- summary(salary_data$Gender)
+print("Summary of Gender:")
+print(gender_summary)
