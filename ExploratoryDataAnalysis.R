06f66ee2d76fe381ca587e3948459a4382e65a91
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-07 14:31:09 +0300

    load dataset for EDA

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -1,1 +1,19 @@
+# Load dataset
+salary_data <- read.csv("data/salary_prediction_data.csv", colClasses = c(
+  Education = "factor",
+  Experience = "integer",
+  Location = "factor",
+  Job_Title = "factor",
+  Age = "integer",
+  Gender = "factor",
+  Salary = "numeric"
+))
 
+# Display the structure of the dataset
+str(salary_data)
+
+# View the first few rows of the dataset
+head(salary_data)
+
+# View the dataset in a separate viewer window
+View(salary_data)
